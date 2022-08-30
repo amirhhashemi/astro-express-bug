71866e53c5f20b34196e01eba2ff351403744017
Arman Hashemi <ahhdev@gmail.com>
2022-08-30 10:21:10 +0430

    add images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node(),
-  integrations: [react()],
+  integrations: [react(), image()]
 });
